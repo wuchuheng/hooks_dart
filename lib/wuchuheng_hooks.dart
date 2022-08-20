58fafde4db8fb2422c75c0fc2e3730dbdb98cad6
wuchuheng <root@wuchuheng.com>
2022-08-20 14:39:10 +0800

    feat: Optimize file export

--- lib/wuchuheng_hooks.dart
+++ lib/wuchuheng_hooks.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library wuchuheng_hooks;
 
 export 'src/index.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/subscription_builder/subscription_builder_abstract.dart';
